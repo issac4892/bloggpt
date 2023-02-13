610ef1afb0b9907c8f83c33baefbeadc63e5365f
issac4892 <issac4892@gmail.com>
2023-02-13 12:25:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,34 +20,34 @@ category_ids = [1]
 def process():
     ai = NotionAI(TOKEN)
     topic = ai.blog_post(
-        "I want you to write out one small but important development common sense topic in Korean in the form of an interrogative sentence. I want a different topic than the ones you've answered before. Only write the first line and never add any additional explanation after it. Don't put any symbols in the sentences, just text. Don't put spaces at the beginning and end of the sentences. Do not put a space at the beginning of a sentence.")
+        "I want you to write out one small but important software development common sense topic in English in the form of an interrogative sentence. I want a different topic than the ones you've answered before. Only write the first line and never add any additional explanation after it. Don't put any symbols in the sentences, just text. Don't put spaces at the beginning and end of the sentences. Do not put a space at the beginning of a sentence.")
     print(f'토픽 생성 완료: {topic}')
 
     article = ai.blog_post(
         f"Write a very long technical article about {topic} in English. When you end your reply, do so naturally, bringing everything to a close. Don't cut off sentences mid-sentence, but finish them. Write at least 50 lines of text. Don't write in markdown, just plain text that will go into a blog post.")
     print(f'아티클 생성 완료: {article}')
-
+    slug = topic.replace(' ','-').lower()
     payload = {
         "status": status,
         "slug": slug,
         "title": topic,
         "content": article,
-        "date": datetime.now.isoformat(),
+        "date": str(datetime.now().isoformat),
         "categories": category_ids
     }
 
     res = requests.post(urljoin(WP_URL, "wp-json/wp/v2/posts"),
-        data=json.dumps(payload),
-        headers = {'Content-type': "application/json"},
-        auth=(WP_USERNAME, WP_PASSWORD)
-    )
+                        data=json.dumps(payload),
+                        headers = {'Content-Type': "application/json"},
+                        auth=(WP_USERNAME, WP_PASSWORD)
+                        )
 
     if res.ok :
         print(f"{res.status.code}")
     else:
-        print(f"error {res.status.code}")
+        print(f"error {res}")
 
-    threading.Timer(3600, process).start()
+    threading.Timer(120, process).start()
 
 
 if __name__ == '__main__':
