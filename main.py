0bbfcff378d0d8aafa42798b5a7c72336755f246
issac4892 <issac4892@gmail.com>
2023-02-13 12:11:00 +0900

    arrangement for wp

--- main.py
+++ main.py
@@ -3,30 +3,49 @@ import requests
 import threading
 from notionai import NotionAI
 from dotenv import load_dotenv
+from datetime import datetime
+from urllib.parse import urljoin
+import json
+
 
 load_dotenv()
 TOKEN = os.environ.get("NOTION_TOKEN")
-TISTORY_ACCESS_TOKEN = os.environ.get("TISTORY_ACCESS_TOKEN")
-TISTORY_BLOG_NAME = os.environ.get("TISTORY_BLOG_NAME")
-
+WP_URL = 'https://testbed.algorix.io'
+WP_USERNAME = 'algorix'
+WP_PASSWORD = os.environ.get("PW")
+status = 'publish'
+slug = 'posts'
+category_ids = [1]
 
 def process():
     ai = NotionAI(TOKEN)
     topic = ai.blog_post(
-        "소소하면서 중요한 개발 상식 주제 1가지를 의문문 문장 형태로 한국어로 출력해줘. 이전에 대답한 주제들과는 다른 주제를 원해. 오직 처음 한 줄로 충력하고 절대로 그 뒤에 아무런 부가 설명도 붙이지 마. 문장에 어떤 기호도 붙이지 말고 텍스트만 출력해. 문장 처음과 끝에 공백을 넣지 마십시오. 문장 가장 처음에 공백을 넣지 마십시오.")
+        "I want you to write out one small but important development common sense topic in Korean in the form of an interrogative sentence. I want a different topic than the ones you've answered before. Only write the first line and never add any additional explanation after it. Don't put any symbols in the sentences, just text. Don't put spaces at the beginning and end of the sentences. Do not put a space at the beginning of a sentence.")
     print(f'토픽 생성 완료: {topic}')
 
     article = ai.blog_post(
-        f"'{topic}'에 대한 기술 관련 글을 한국어로 매우 길게 작성하세요. 답장을 끝낼때는 모든 상황을 마무리 지으면서 자연스럽게 끝내십시오. 문장을 중간에 끊지 말고 끝까지 이으세요. 적어도 50줄 이상의 글을 작성하세요. 마크다운 식으로 작성하지 말고, 블로그 글에 들어갈 일반 텍스트들로만 작성하세요.")
+        f"Write a very long technical article about {topic} in English. When you end your reply, do so naturally, bringing everything to a close. Don't cut off sentences mid-sentence, but finish them. Write at least 50 lines of text. Don't write in markdown, just plain text that will go into a blog post.")
     print(f'아티클 생성 완료: {article}')
 
-    tags = ai.blog_post(
-        f"'{topic}'에 대한 적절한 태그를 10개 이상 50개 이하로 작성해주세요. ','로 구분합니다. 한글로 작성하고 한 태그는 하나의 단어로 이루어져 있습니다. '#'을 사용하지 않고 단어만 작성하십시오. ',' 뒤에 공백을 넣지 말고 다음 태그와 붙여서 쓰십시오.")
-    print(f'태그 생성 완료: {tags}')
+    payload = {
+        "status": status,
+        "slug": slug,
+        "title": topic,
+        "content": article,
+        "date": datetime.now.isoformat(),
+        "categories": category_ids
+    }
 
-    requests.post(
-        f"https://www.tistory.com/apis/post/write?access_token={TISTORY_ACCESS_TOKEN}&blogName={TISTORY_BLOG_NAME}&title={topic}&visibility=3&content={article}&tag={tags}")
-    print('아티클 업로드 완료!')
+    res = requests.post(urljoin(WP_URL, "wp-json/wp/v2/posts"),
+        data=json.dumps(payload),
+        headers = {'Content-type': "application/json"},
+        auth=(WP_USERNAME, WP_PASSWORD)
+    )
+
+    if res.ok :
+        print(f"{res.status.code}")
+    else:
+        print(f"error {res.status.code}")
 
     threading.Timer(3600, process).start()
 
